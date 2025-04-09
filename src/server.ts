cc34989b7daaa8978b6553a37827f16b174a99d5
A1n2F <alissonfferreira22@gmail.com>
2025-04-09 12:06:22 -0300

    Implementado os metodos Select, Update, Delete

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
 import { knex } from "./database/knex"
+import { request } from "http"
 
 const app = express()
 app.use(express.json())
@@ -15,7 +16,33 @@ app.post("/courses", async (request: Request, response: Response) => {
 
   await knex.raw("INSERT INTO courses (name) VALUES (?)", [name])
 
-  response.status(201).json()
+  return response.status(201).json()
+})
+
+app.get("/courses", async (request: Request, response: Response) => {
+  //const courses = await knex.raw("SELECT * FROM courses")
+
+  const courses = await knex("courses").select().orderBy("name")
+
+  return response.json(courses)
+})
+
+app.put("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params
+  const { name } = request.body
+
+  await knex("courses").update({ name }).where({ id })
+
+
+  return response.json()
+})
+
+app.delete("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params
+
+  await knex("courses").delete().where({id})
+
+  return response.json()
 })
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))
